648d16688c86a16384eb89dba9c69b8dc485f28e
monibbormon <monibbormon14@gmail.com>
2022-03-26 11:05:38 +0600

    verify token

--- config/auth.js
+++ config/auth.js
@@ -5,7 +5,7 @@ module.exports = {
         'tokenURL': 'https://auth.pingone.eu/68739399-369f-4182-bde5-d06ef0a2f0b3/as/token',
         'clientID': '98010a1f-46ba-482b-a604-2974570395f0',
         'clientSecret': '64fET4Cj3On4f0V1LL1YgNCrIHQmxnUyB5Pe0RTxOiNfIkiiZCwU6y2NcSK8taUS',
-        'callbackURL': 'http://localhost:5000/users',
+        'callbackURL': 'http://localhost:8080',
     }
 
 };
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const cors = require('cors')
-var passport = require('passport');
-var OAuth2Strategy = require('passport-ping-oauth2').Strategy;
 var configAuth = require('./config/auth');
+const JWT = require('jsonwebtoken')
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,46 +14,28 @@ app.use(express.json())
 // verify Access Token 
 async function verifyToken(req, res, next) {
     if (req.headers?.authorization?.startsWith('Bearer ')) {
-        let idToken = req.headers?.authorization?.split('Bearer ')[1]
-        console.log(idToken)
-        passport.use(new OAuth2Strategy({
-            authorizationURL: configAuth.pingAuth.authorizationURL,
-            tokenURL: configAuth.pingAuth.tokenURL,
-            clientID: configAuth.pingAuth.clientID,
-            clientSecret: configAuth.pingAuth.clientSecret,
-            callbackURL: configAuth.pingAuth.callbackURL
-        },
-            function (idToken, refreshToken, params, profile, done) {
+        var idToken = req.headers?.authorization?.split('Bearer ')[1]
+        // console.log(idToken)
+        try {
+            const decoded = JWT.decode(idToken)
+            decoded.client_id = configAuth.pingAuth.clientID
+        } catch {
+            res.status(401).json({
+                message: "Not authorized."
+            })
+        }
 
-                User.findOne({ 'ping.id': profile.cn }, function (err, user) {
-                    if (err) {
-                        return done(err);
-                    }
-                    if (user) {
-                        return done(null, user);
-                    } else {
-                        var newUser = new User();
-                        newUser.ping.id = profile.id;
-                        newUser.ping.token = idToken;
-                        newUser.ping.name = profile.displayName;
-                        newUser.ping.email = profile.email;
-                        newUser.save(function (err) {
-                            console.log(newUser)
-                            if (err) { throw err; }
-                            return done(null, newUser);
-                        });
-                    }
-                });
-            }));
     }
     next()
 }
 
 
-app.post('/users', verifyToken, async (req, res) => {
-    console.log('success')
-})
 
+app.post('/users', verifyToken, async (req, res) => {
+    res.json({
+        "verify": "Verfied."
+    })
+})
 
 
 
